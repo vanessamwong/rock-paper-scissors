6e3ffb54eaaf58489794c319ff41d9a937987bca
vanessamwong <vanessamwong19@gmail.com>
2023-01-16 21:39:23 -0500

    Fixed socring system and buttons

--- script.js
+++ script.js
@@ -1,29 +1,72 @@
 let playerScore = 0;
 let computerScore = 0;
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const result = document.querySelector(".prompt");
+const your_score = document.getElementById("player");
+const cpu_score = document.getElementById("computer");
 
 const message = document.createElement("div");
 
 function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissors"];
-    return options[Math.floor(Math.random() * 2)];
+    return options[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function win(playerSelection, computerSelection) {
+    playerScore++;
+    your_score.innerHTML = "PLAYER: " + playerScore;
+    cpu_score.innerHTML = "COMPUTER: " + computerScore;
+    result.innerHTML = playerSelection + " beats " + computerSelection + "! You win!";
+}
 
-    if (playerSelection == computerSelection) {
-        playerScore = playerScore;
-        computerScore = computerScore;
-    } else if (((playerSelection == "Rock") && (computerSelection == "Paper")) ||
-               ((playerSelection == "Scissors") && (computerSelection == "Rock")) ||
-               ((playerSelection == "Paper") && (computerSelection == "Scissors"))) {
-                computerScore++;
-                document.getElementById("computer").innerHTML = "Computer: " + computerScore;
-    } else {
-        playerScore++;
-        document.getElementById("player").innerHTML = "Player: " + playerScore;
+function lose(playerSelection, computerSelection) {
+    computerScore++;
+    your_score.innerHTML = "PLAYER: " + playerScore;
+    cpu_score.innerHTML = "COMPUTER: " + computerScore;
+    result.innerHTML = computerSelection + " beats " + playerSelection + "! You lose!";
+}
+
+function tie(playerSelection, computerSelection) {
+    your_score.innerHTML = "PLAYER: " + playerScore;
+    cpu_score.innerHTML = "COMPUTER: " + computerScore;
+    result.innerHTML = "Tie game!";
+}
+
+function game (playerSelection) {
+    const computerSelection = getComputerChoice();
+    switch (playerSelection + computerSelection) {
+        case "RockScissors":
+        case "PaperRock":
+        case "ScissorsPaper":
+            win(playerSelection, computerSelection);
+            break;
+        case "RockPaper":
+        case "PaperScissors":
+        case "ScissorsRock":
+            lose(playerSelection, computerSelection);
+            break;
+        case "RockRock":
+        case "PaperPaper":
+        case "ScissorsScissors":
+            tie(playerSelection, computerSelection);
+            break;
     }
 }
 
+function main() {
+    rock.addEventListener("click", function() {
+        game("Rock");
+    })
+    paper.addEventListener("click", function() {
+        game("Paper");
+    })
+    scissors.addEventListener("click", function() {
+        game("Scissors");
+    })
+
+}
 
 
-
+main();
